1bcc47511dd38ee151fe98bb1fb36ba77dfbe987
Irina444444 <staat4444@gmail.com>
2022-04-09 20:00:43 +0300

    Add if condition

---
+++ Example005_IfCondition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Add name of user");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("My dear Maria!");    
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
